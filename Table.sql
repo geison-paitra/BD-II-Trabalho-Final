e21f65b5add2ae09b01fd5b7f7c8a98ebc79d197
geisonplays <geison.dge@gmail.com>
2015-11-17 01:11:55 -0200

    Script BD

---
+++ Table.sql
@@ -0,0 +1,25 @@
+﻿-- CRIANDO TABELAS
+CREATE TABLE Bairro (
+	id serial,
+	nome text,
+	coordenadas Polygon
+);
+ALTER TABLE Bairro ADD PRIMARY KEY (id);
+
+CREATE TABLE Tipo_ocorrencia (
+	id serial,
+	tipo_ocorrencia text
+);
+ALTER TABLE Tipo_ocorrencia ADD PRIMARY KEY (id);
+
+CREATE TABLE Ocorrencias (
+	id serial,
+	descricao text,
+	coordenada Point,
+	tipo_ocorrencia integer
+);
+ALTER TABLE Ocorrencias ADD PRIMARY KEY (id);
+ALTER TABLE Ocorrencias ADD FOREIGN KEY (tipo_ocorrencia) REFERENCES Tipo_ocorrencia (id);
+
+
+-- 
